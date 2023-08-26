5dc4078bb77a61365caa892333592a481c352426
Zydak <koksszmoks@gmail.com>
2023-08-26 09:58:53 +0200

    Windows build

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,18 @@
 project "portaudio"
+    architecture "x86_64"
     kind "StaticLib"
     language "C"
+    location "build"
+    configurations { "Debug", "Release", "Dist" }
+    platforms { "Windows", "Linux" 
 
     objdir "build/obj/%{cfg.buildcfg}"
     targetdir "build/bin/%{cfg.buildcfg}"
 
-    prebuildcommands
-    {
-        "./configure",
-        "make",
-        "cp lib/.libs/libportaudio.a ../lib/liblibportaudio.a" -- for some reason it has to be liblib
-    }
+    filter "platforms:Linux"
+        prebuildcommands
+        {
+            "./configure",
+            "make",
+            "cp lib/.libs/libportaudio.a ../lib/liblibportaudio.a" -- for some reason it has to be liblib
+        }
